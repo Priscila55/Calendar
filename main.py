9ff0b5827fe21c4e3ea0373d2e6df15676d25868
Priscila Pirtac <117281138+Priscila55@users.noreply.github.com>
2025-09-04 18:10:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,5 @@ app = create_app()
 
 # only if we run the file we will execute the line below
 if __name__ == '__main__':
-    app.run(debug=True) # run flask application
+    app.run(debug=True, port = 5002) # run flask application
     
-    
-
